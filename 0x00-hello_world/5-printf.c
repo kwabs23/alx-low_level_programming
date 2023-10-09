67384b57b6d88e5a00860f7b062e71f3e632f4ff
Kwabena Duah <kaduah079@gmail.com>
2023-10-09 02:03:40 -0700

    Printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  * main - this is a printf fuction
  *
- * Return: 0
+ * Return: 0 when succesful
  */
 
 int main(void)
 {
-	printf("with proper grammar but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
