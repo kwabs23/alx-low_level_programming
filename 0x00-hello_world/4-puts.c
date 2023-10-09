254b53f9440108debbbc21f7089fd4e262978365
Kwabena Duah <kaduah079@gmail.com>
2023-10-08 17:09:16 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
 /**
- *  * main - this is the main put function
- *   * Return: 0
- *    */
+ * main - this is the main put function
+ *
+ * Return: 0
+ */
+
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
