4eae0c105a4153829d66c91433f89102ef79996c
Kwabena Duah <kaduah079@gmail.com>
2023-10-11 19:38:21 -0700

    Natural numbers

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,21 +1,15 @@
-#include "main.h"
-
-
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
-
+#include <stdio.h>
 
 int main(void)
 {
-	print_times_table(3);
-	_putchar('\n');
-	print_times_table(5);
-	_putchar('\n');
-	print_times_table(98);
-	_putchar('\n');
-	print_times_table(12);
-	return (0);
+	int sum = 0;
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum = sum + i;
+	}
+
+	printf("%d\n", sum);
 }
