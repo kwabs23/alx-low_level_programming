dec37a65c05cd999acd243994057ecce1b9dde41
Kwabena Duah <kaduah079@gmail.com>
2023-10-24 04:48:17 -0700

    Memset correction

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _memset - main function of the prototype
  *
@@ -11,11 +10,14 @@
  *
  * Return: s
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int a;
 
 	for (a = 0; a < n; a++)
-	s[a] = b;
-	return (s);
+	{
+		s[a] = b;
+	}
+		return (s);
 }
