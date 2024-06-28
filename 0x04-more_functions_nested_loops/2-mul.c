aec9ec04bc2c96c9b957d50a14c6e91d1fe2bcf5
kwabs23 <kaduah079@gmail.com>
2024-06-28 15:02:29 -0700

    Task 2 more functions nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - function that multuplies two integers
+ * @a: parameter 1
+ * @b: parameter 2
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
